d7cd61ece4b83212e2144a96a2623e13db696940
monscerrat gutierrez <monscerratgutierrez@MacBook-Air-de-monscerrat.local>
2024-05-16 23:13:47 -0600

    first commit

---
+++ fantasyRole/fantasyRoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fantasyRoleApp.swift
+//  fantasyRole
+//
+//  Created by monscerrat gutierrez on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct fantasyRoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
